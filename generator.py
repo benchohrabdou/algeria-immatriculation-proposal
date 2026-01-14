2669e8451883d342148c34aae4e6f23ee898d00c
Benchohra Mohamed Abderrahmane <abdoubench236@gmail.com>
2026-01-14 10:36:39 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,6 +2,20 @@ import string
 
 ALPHABET = string.ascii_uppercase
 
+def encode_year(year):
+    """
+    Convertit une année réelle (1985–2084) en code 2 chiffres.
+    Exemple :
+    1985 -> 85
+    2000 -> 00
+    2084 -> 84
+    """
+    if year < 1985 or year > 2084:
+        raise ValueError("L'année doit être comprise entre 1985 et 2084.")
+
+    return f"{year % 100:02d}"
+
+
 def to_base26(num, length=6):
     """Convertit un entier en base 26 (A-Z) avec padding."""
     if num < 0:
@@ -13,7 +27,7 @@ def to_base26(num, length=6):
         letters.append(ALPHABET[remainder])
         if num == 0:
             break
-        num -= 1
+        num -= 1  # correction
 
     result = ''.join(reversed(letters))
     return result.rjust(length, 'A')
@@ -24,12 +38,12 @@ def generate_plate(vehicle_type, year, serial_number):
     if not (0 <= vehicle_type <= 9):
         raise ValueError("Le type doit être entre 0 et 9")
 
-    year = str(year)
+    year_code = encode_year(year)
     base26_serial = to_base26(serial_number)
 
-    return f"{vehicle_type}-{year}-{base26_serial}"
+    return f"{vehicle_type}-{year_code}-{base26_serial}"
 
 
 if __name__ == "__main__":
-    print(generate_plate(0, 2024, 152347))
+    print(generate_plate(0, 2024, 152347))  # exemple
     print(generate_plate(2, 2019, 42))
