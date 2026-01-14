b398f674a86319864022c8d30f70552ae42f5a34
Benchohra Mohamed Abderrahmane <abdoubench236@gmail.com>
2026-01-14 10:37:04 +0100

    Update vehicle registration generator prompts

--- CLI_main.py
+++ CLI_main.py
@@ -1,9 +1,9 @@
 from generator import generate_plate
 
-print("=== Générateur de Matricules Algérien (Proposition) ===")
+print("=== Générateur de Matricules Algériens (Format Moderne) ===")
 
 vehicle_type = int(input("Type du véhicule (0-9) : "))
-year = int(input("Année : "))
+year = int(input("Année (1985–2084) : "))
 serial = int(input("Numéro de série : "))
 
 plate = generate_plate(vehicle_type, year, serial)
